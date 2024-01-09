d99a69e562eb6a0f15252241d8277a12f8ab52f2
mohamadreza bigdeli <bigdelim80@gmail.com>
2024-01-09 14:59:45 +0330

    initial database structure

--- anakonda/anakonda.py
+++ anakonda/anakonda.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, Flask
+from flask_migrate import Migrate
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
 from .config import Config
 
 apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
+db = SQLAlchemy()
+mg = Migrate()
 
 
 def create_app():
@@ -12,5 +16,7 @@ def create_app():
 
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
+    mg.init_app(app, db)
     app.register_blueprint(apiv1_bp)
     return app
