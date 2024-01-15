bf5859499996f65f169edf7693685bab4eb1da8f
mohamadreza bigdeli <bigdelim80@gmail.com>
2024-01-15 06:36:02 +0330

    update jsonify codes

--- anakonda/util/jsonify.py
+++ anakonda/util/jsonify.py
@@ -1,6 +1,17 @@
 from anakonda.config import Config
 
-STATUS_MESSAGE = {100: "OK", 101: "Method is not implemented"}
+STATUS_MESSAGE = {
+    100: "OK",
+    101: "Method is not implemented",
+    102: "Schema validation failed",
+    103: "Schema instance error",
+    104: "Invalid input provided",
+    105: "Media type is not supported",
+    106: "Database error",
+    107: "Resource is not found",
+    108: "Runtime is not available",
+    109: "Resource is not updatable",
+}
 
 
 def jsonify(state={}, metadata={}, status=200, code=100, headers={}):
@@ -9,6 +20,7 @@ def jsonify(state={}, metadata={}, status=200, code=100, headers={}):
     resource["metadata"] = metadata
     resource["status"] = {
         "code": code,
+        # show message just when ANAKONDA_API_DEBUG=1 => develop usage
         "message": STATUS_MESSAGE[code] if Config.DEBUG else None,
     }
     return resource, status, headers
